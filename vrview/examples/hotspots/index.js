573187f15518097292a6b1a00b391d964f906fad
Freyli <Freyli@users.noreply.github.com>
2016-12-18 19:54:08 -0500

    next step

--- vrview/examples/hotspots/index.js
+++ vrview/examples/hotspots/index.js
@@ -44,12 +44,6 @@ var scenes = {
     image: 'stair2.jpg',
     preview: 'stair2-preview.jpg',
     hotspots: {
-      stair0: {
-        pitch: 0,
-        yaw: 125,
-        radius: 0.05,
-        distance: 1
-      },
       livingroom: {
         pitch: 0,
         yaw: 110,
@@ -61,9 +55,95 @@ var scenes = {
         yaw: 30,
         radius: 0.05,
         distance: 1
+      },
+      bathroom: {
+        pitch: 0,
+        yaw: 20,
+        radius: 0.05,
+        distance: 1
+      },
+      bedroom: {
+        pitch: 0,
+        yaw: 40,
+        radius: 0.05,
+        distance: 1
+      },
+      office: {
+        pitch: 0,
+        yaw: 90,
+        radius: 0.05,
+        distance: 1
+      }
+
+    }
+  },
+  
+  bathroom: {
+    image: 'bathroom.jpg',
+    preview: 'bathroom-preview.jpg',
+    hotspots: {
+      stair2: {
+        pitch: 0,
+        yaw: 305,
+        radius: 0.05,
+        distance: 1
+      },
+  bedroom: {
+    image: 'bedroom.jpg',
+    preview: 'bedroom-preview.jpg',
+    hotspots: {
+      stair2: {
+        pitch: 0,
+        yaw: 305,
+        radius: 0.05,
+        distance: 1
+      }
+     }
+  }, 
+  office: {
+    image: 'office.jpg',
+    preview: 'office-preview.jpg',
+    hotspots: {
+      stair2: {
+        pitch: 0,
+        yaw: 305,
+        radius: 0.05,
+        distance: 1
+      }
+      
+     }
+  },  
+   basement: {
+    image: 'basement.jpg',
+    preview: 'basement-preview.jpg',
+    hotspots: {
+      stair0: {
+        pitch: 0,
+        yaw: 305,
+        radius: 0.05,
+        distance: 1
+      },
+      workshop: {
+        pitch: 0,
+        yaw: 200,
+        radius: 0.05,
+        distance: 1
+      }
+    }
+  }, 
+  workshop: {
+    image: 'workshop.jpg',
+    preview: 'workshop-preview.jpg',
+    hotspots: {
+      basement: {
+        pitch: 0,
+        yaw: 305,
+        radius: 0.05,
+        distance: 1
       }
     }
   },
+      
   stair0: {
     image: 'stair0.jpg',
     preview: 'stair0-preview.jpg',
@@ -74,7 +154,7 @@ var scenes = {
         radius: 0.05,
         distance: 1
       },
-      stair2: {
+      basement: {
         pitch: 0,
         yaw: 180,
         radius: 0.05,
